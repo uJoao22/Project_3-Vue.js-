10acc918c96f9d36b66c5e8afd4adef94e4a9a73
joaoBotelho22 <joaopedro@saudeagora.com.vc>
2021-12-22 11:29:59 -0300

    ALterando propriedades através dos testes

--- vue-test-master/src/tests/unit/Lance.spec.js
+++ vue-test-master/src/tests/unit/Lance.spec.js
@@ -1,49 +1,87 @@
 import Lance from '@/components/Lance'
 import { mount } from '@vue/test-utils'
 
-//Testando o cenario de valor invalido, onde o evento não deve ser emitido para valores invalidos, menores que zero
-test('Não aceita lance com valor menor do que zero', () => { //O primeiro parametro é a descrição do que iremos testar, o segundo parametro é a função que irá conter o teste
-    const wrapper = mount(Lance) //Montando o componente Lance
+describe('Um lance sem valor minimo', () => {
+    //Testando o cenario de valor invalido, onde o evento não deve ser emitido para valores invalidos, menores que zero
+    test('Não aceita lance com valor menor do que zero', () => { //O primeiro parametro é a descrição do que iremos testar, o segundo parametro é a função que irá conter o teste
+        const wrapper = mount(Lance) //Montando o componente Lance
 
-    const input = wrapper.find('input') //Capturando o input dentro do componente Lance
-    input.setValue(-100) //Defininfo um valor invalido para o input
+        const input = wrapper.find('input') //Capturando o input dentro do componente Lance
+        input.setValue(-100) //Defininfo um valor invalido para o input
 
-    const lancesEmitidos = wrapper.emitted('novo-lance') //Ouvindo todos as emissões de evento, do evento 'novo-lance'
-    wrapper.trigger('submit') //Usando o trigger para ativar o evento de submit do formulario
+        const lancesEmitidos = wrapper.emitted('novo-lance') //Ouvindo todos as emissões de evento, do evento 'novo-lance'
+        wrapper.trigger('submit') //Usando o trigger para ativar o evento de submit do formulario
 
-    expect(lancesEmitidos).toBeUndefined() //Esperando que lancesEmitidos retornem undefined
+        expect(lancesEmitidos).toBeUndefined() //Esperando que lancesEmitidos retornem undefined
+    })
+
+    //Testando o cenario de sucesso, onde o evento deve ser chamado, pois existe um valor maior do que zero
+    test('Emite um lance quando o valor é maior do que zero', () => {
+        const wrapper = mount(Lance)
+
+        const input = wrapper.find('input')
+        input.setValue(100) //Defininfo um valor valido para o input
+
+        wrapper.trigger('submit')
+
+        const lancesEmitidos = wrapper.emitted('novo-lance')
+
+        expect(lancesEmitidos).toHaveLength(1) //Esperando que lancesEmitidos retornem um tamanho 1, que exista elementos dentro dele
+    })
+
+    //Garantindo que o valor emito do lance, seja o mesmo inserindo em nosso componente
+    test('Emite o valor esperado de um lance válido', () => {
+        const wrapper = mount(Lance)
+
+        const input = wrapper.find('input')
+        input.setValue(100)
+
+        wrapper.trigger('submit')
+
+        const lancesEmitidos = wrapper.emitted('novo-lance')
+
+        // Resultado emitido pelo lancesEmitidos
+        // [
+        //     [100]
+        // ]
+
+        const lance = parseInt(lancesEmitidos[0][0]) //Pegando o resultado de lancesEmitidos para conferir se é o mesmo inserido no componente
+        expect(lance).toBe(100) //Esperando que o valor de lance seja 100, o mesmo que foi inserido no input
+    })
 })
 
-//Testando o cenario de sucesso, onde o evento deve ser chamado, pois existe um valor maior do que zero
-test('Emite um lance quando o valor é maior do que zero', () => {
-    const wrapper = mount(Lance)
+describe('Um lance com valor minimo', () => {
+    //Delimitando um valor minimo para o lance
+    test('Todos os lances devem possuir um valor maior do que o minimo informado', () => {
+        const wrapper = mount(Lance, { //Enviando como parametro um objeto JS com valores de propriedades
+            propsData: 300 //Enviando propriedades para nosso componente Lance
+        })
 
-    const input = wrapper.find('input')
-    input.setValue(100) //Defininfo um valor valido para o input
+        const input = wrapper.find('input')
+        input.setValue(400) //Atribuindo um valor valido
 
-    wrapper.trigger('submit')
+        wrapper.trigger('submit')
 
-    const lancesEmitidos = wrapper.emitted('novo-lance')
+        const lancesEmitidos = wrapper.emitted('novo-lance')
 
-    expect(lancesEmitidos).toHaveLength(1) //Esperando que lancesEmitidos retornem um tamanho 1, que exista elementos dentro dele
-})
+        expect(lancesEmitidos).toHaveLength(1) //Esperamos que o eveneto tenha sido invocado uma vez pra o lance
+    })
 
-//Garantindo que o valor emito do lance, seja o mesmo inserindo em nosso componente
-test('Emite o valor esperado de um lance válido', () => {
-    const wrapper = mount(Lance)
+    //Garantindo que o valor emitido seja o valor inserido
+    test('emite o valor esperado de um lance valido', () => {
+        const wrapper = mount(Lance, {
+            propsData: 300
+        })
 
-    const input = wrapper.find('input')
-    input.setValue(100)
+        const input = wrapper.find('input')
+        input.setValue(400)
 
-    wrapper.trigger('submit')
+        wrapper.trigger('submit')
 
-    const lancesEmitidos = wrapper.emitted('novo-lance')
+        const lancesEmitidos = wrapper.emitted('novo-lance')
 
-    // Resultado emitido pelo lancesEmitidos
-    // [
-    //     [100]
-    // ]
+        const valorDoLance = parseInt(lancesEmitidos[0][0])
 
-    const lance = parseInt(lancesEmitidos[0][0]) //Pegando o resultado de lancesEmitidos para conferir se é o mesmo inserido no componente
-    expect(lance).toBe(100) //Esperando que o valor de lance seja 100, o mesmo que foi inserido no input
+        expect(valorDoLance).toBe(400) //Esperamos que o valor emitido no lance seja o mesmo que o valor inserido no input
+    })
 })
