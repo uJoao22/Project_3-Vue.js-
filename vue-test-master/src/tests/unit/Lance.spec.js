ba7c43f92785b366865eececbb51493590bb81ab
joaoBotelho22 <joaopedro@saudeagora.com.vc>
2021-12-22 10:41:38 -0300

    Encontrando elementos do HTML

--- vue-test-master/src/tests/unit/Lance.spec.js
+++ vue-test-master/src/tests/unit/Lance.spec.js
@@ -1,8 +1,12 @@
 import Lance from '@/components/Lance'
 import { mount } from '@vue/test-utils'
 
-//Montando um teste para verificar se o componente Lance, foi montado corretamente
+//Encontrando elementos do HTML
 test('Não aceita lance com valor menor do que zero', () => { //O primeiro parametro é a descrição do que iremos testar, o segundo parametro é a função que irá conter o teste
     const wrapper = mount(Lance) //Retornando para a variavel o empacotador do mount
-    expect(wrapper).toBeTruthy() //Esperamos que wrapper seja verdadeiro, tenha sido montado com sucesso
+
+    const input = wrapper.find('input') //Usando o método find para buscar elemento do HTML, como o input
+    input.setValue(-100)
+
+    expect(input).toBeTruthy() //Esperamos que wrapper seja verdadeiro, tenha sido montado com sucesso
 })
